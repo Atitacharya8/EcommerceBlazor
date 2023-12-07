d87cdb377121e2c8597addb1f9945e5db0f9084e
atitacharya8 <atitacharya8@gmail.com>
2023-12-07 14:45:31 -0600

    ICategoryRepository Added

---
+++ Ecommerce_Business/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using Ecommerce_Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce_Business.Repository.IRepository
+{
+    public interface ICategoryRepository
+    {
+        public Create(CategoryDTO);
+    }
+}
