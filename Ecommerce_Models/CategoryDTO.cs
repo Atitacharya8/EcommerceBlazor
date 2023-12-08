9afd6eef58941d3120f9df6fd86bd5b87491f014
atitacharya8 <atitacharya8@gmail.com>
2023-12-07 19:03:39 -0600

    Validations in Blazor Form

--- Ecommerce_Models/CategoryDTO.cs
+++ Ecommerce_Models/CategoryDTO.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Ecommerce_Models
 {
     public class CategoryDTO
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage ="Please enter name")]
         public string Name { get; set; }
     }
 }
