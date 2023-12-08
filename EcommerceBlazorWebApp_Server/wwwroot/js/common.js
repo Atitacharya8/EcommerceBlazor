b6335c80b425cd3eb0b6ff5b9b7cd8ae878873fb
atitacharya8 <atitacharya8@gmail.com>
2023-12-07 20:46:26 -0600

    Delete Confirmation Component implemented

--- EcommerceBlazorWebApp_Server/wwwroot/js/common.js
+++ EcommerceBlazorWebApp_Server/wwwroot/js/common.js
@@ -25,4 +25,14 @@ window.ShowSwal = (type, message) => {
         );
     }
 }
+
+function ShowDeleteConfirmationModal() {
+
+    $('#deleteConfirmationModal').modal('show');
+}
+
+function HideDeleteConfirmationModal() {
+
+    $('#deleteConfirmationModal').modal('hide');
+}
  
