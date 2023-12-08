7fc666b0d00e01fbd43cf9fb0b90636832711080
atitacharya8 <atitacharya8@gmail.com>
2023-12-07 22:22:34 -0600

    all methods are changed to async

--- Ecommerce_Business/Repository/CategoryRepository.cs
+++ Ecommerce_Business/Repository/CategoryRepository.cs
@@ -3,6 +3,7 @@ using Ecommerce_Business.Repository.IRepository;
 using Ecommerce_DataAccess;
 using Ecommerce_DataAccess.Data;
 using Ecommerce_Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Ecommerce_Business.Repository
 {
@@ -17,7 +18,7 @@ namespace Ecommerce_Business.Repository
             _mapper = mapper;
 
         }
-        public CategoryDTO Create(CategoryDTO objDTO) //CategoryDTO must be accessed by converting to its corresponding category object
+        public async Task<CategoryDTO> Create(CategoryDTO objDTO) //CategoryDTO must be accessed by converting to its corresponding category object
         {
             // convert from CategoryDTO to Category with source = objDTO
             var obj = _mapper.Map<CategoryDTO, Category>(objDTO);
@@ -28,25 +29,25 @@ namespace Ecommerce_Business.Repository
             var addedObj= _db.Categories.Add(obj);
 
             //this method must be used otherwise the data will not be stored in database
-            _db.SaveChanges(); 
+            await _db.SaveChangesAsync(); 
 
             return _mapper.Map<Category, CategoryDTO>(addedObj.Entity); //source is addedObj
         }
 
-        public int Delete(int id)
+        public async Task<int> Delete(int id)
         {
-            var obj = _db.Categories.FirstOrDefault(c=>c.Id == id);
+            var obj = await _db.Categories.FirstOrDefaultAsync(c=>c.Id == id);
             if(obj != null)
             {
                 _db.Categories.Remove(obj);
-                return _db.SaveChanges();
+                return await _db.SaveChangesAsync();
             }
             return 0;
         }
 
-        public CategoryDTO Get(int id)
+        public async Task<CategoryDTO> Get(int id)
         {
-            var obj = _db.Categories.FirstOrDefault(c => c.Id == id);
+            var obj = await _db.Categories.FirstOrDefaultAsync(c => c.Id == id);
             if (obj != null)
             {
                 
@@ -57,19 +58,19 @@ namespace Ecommerce_Business.Repository
             return new CategoryDTO();
         }
 
-        public IEnumerable<CategoryDTO> GetAll()
+        public async Task<IEnumerable<CategoryDTO>> GetAll()
         {
             return _mapper.Map<IEnumerable<Category>, IEnumerable<CategoryDTO>>(_db.Categories); //source will be retrieved using _db.Categories
         }
 
-        public CategoryDTO Update(CategoryDTO objDTO)
+        public async Task<CategoryDTO> Update(CategoryDTO objDTO)
         {
-            var objFromDb = _db.Categories.FirstOrDefault(c => c.Id == objDTO.Id);
+            var objFromDb = await _db.Categories.FirstOrDefaultAsync(c => c.Id == objDTO.Id);
             if( objFromDb != null )
             {
                 objFromDb.Name=objDTO.Name; //we only use name here so id and createddate are not used
                 _db.Categories.Update(objFromDb);
-                _db.SaveChanges();
+                await _db.SaveChangesAsync();
                 return _mapper.Map<Category, CategoryDTO>(objFromDb);
                 
             }
