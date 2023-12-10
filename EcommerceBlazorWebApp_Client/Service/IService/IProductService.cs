a3da4a2b48a68562ebd5ce396f20a37922290490
atitacharya8 <atitacharya8@gmail.com>
2023-12-10 16:13:08 -0600

    Product service interface to connect web api to client

---
+++ EcommerceBlazorWebApp_Client/Service/IService/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using Ecommerce_Models;
+
+namespace EcommerceBlazorWebApp_Client.Service.IService
+{
+    public interface IProductService
+    {
+        public Task<IEnumerable<ProductDTO>> GetAll();
+
+        public Task<ProductDTO> Get(int productId);
+    }   
+}
---
+++ EcommerceBlazorWebApp_Client/Service/ProductService.cs
@@ -0,0 +1,18 @@
+﻿using Ecommerce_Models;
+using EcommerceBlazorWebApp_Client.Service.IService;
+
+namespace EcommerceBlazorWebApp_Client.Service
+{
+    public class ProductService : IProductService
+    {
+        public Task<ProductDTO> Get(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ProductDTO>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
