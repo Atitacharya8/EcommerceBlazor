67923cc577efd269b5d44c3f0391cd8701f1e4b1
atitacharya8 <atitacharya8@gmail.com>
2023-12-10 23:29:33 -0600

    Cart service implmented

--- EcommerceBlazorWebApp_Client/Service/CartService.cs
+++ EcommerceBlazorWebApp_Client/Service/CartService.cs
@@ -1,18 +1,75 @@
-﻿using EcommerceBlazorWebApp_Client.Service.IService;
+﻿using Blazored.LocalStorage;
+using Ecommerce_Common;
+using EcommerceBlazorWebApp_Client.Service.IService;
 using EcommerceBlazorWebApp_Client.ViewModels;
 
 namespace EcommerceBlazorWebApp_Client.Service
 {
+
     public class CartService : ICartService
     {
-        public Task DecrementCart(ShoppingCart shoppingCart)
+        private readonly ILocalStorageService _localStorageService;
+        public CartService(ILocalStorageService localStorageService)
         {
-            throw new NotImplementedException();
+            _localStorageService= localStorageService;
         }
 
-        public Task IncrementCart(ShoppingCart shoppingCart)
+        public async Task DecrementCart(ShoppingCart cartToDecrement)
         {
-            throw new NotImplementedException();
+            //get all of the items in cart variable
+            var cart = await _localStorageService.GetItemAsync<List<ShoppingCart>>(SD.ShoppingCart);
+                
+            //if count is 0 or 1 then we remove the item
+            for(int i =0; i<cart.Count; i++)
+            {
+                if (cart[i].ProductId == cartToDecrement.ProductId && cart[i].ProductPriceId == cartToDecrement.ProductPriceId)
+                {
+                    if (cart[i].Count==1 || cart[i].Count==0) 
+                    {
+                        cart.Remove(cart[i]);
+                    }
+                    else
+                    {
+                        cart[i].Count -= cartToDecrement.Count; // cart[i].Count =  cart[i].Count - cartToDecrement.Count;
+                    }
+                   
+                }
+            }
+            await _localStorageService.SetItemAsync(SD.ShoppingCart, cart);
+        }
+
+        public async Task IncrementCart(ShoppingCart cartToAdd)
+        {
+            var cart = await _localStorageService.GetItemAsync<List<ShoppingCart>>(SD.ShoppingCart);
+            bool itemInCart = false;
+
+            if(cart == null)
+            {
+                cart = new List<ShoppingCart>();
+            }
+            
+            foreach( var obj in cart) {
+                if(obj.ProductId == cartToAdd.ProductId && obj.ProductPriceId == cartToAdd.ProductPriceId)
+                {
+                    itemInCart = true;
+                    obj.Count += cartToAdd.Count; // obj.Count =  obj.Count + cartToAdd.Count;
+                }
+            }
+
+            //if there are no items in cart add new
+
+            if(!itemInCart) {
+
+                cart.Add(new ShoppingCart()
+                {
+                    ProductId = cartToAdd.ProductId,
+                    ProductPriceId = cartToAdd.ProductPriceId,
+                    Count = cartToAdd.Count
+
+                });
+            }
+
+            await _localStorageService.SetItemAsync(SD.ShoppingCart, cart);
         }
     }
 }
---
+++ Ecommerce_Common/SD.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce_Common
+{
+    public class SD
+    {
+        public const string ShoppingCart = "ShoppingCart";
+    }
+}
