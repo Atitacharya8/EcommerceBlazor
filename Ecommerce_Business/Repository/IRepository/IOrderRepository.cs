0a22cbbc633f4b2beac05af4d748933a7b10c5ac
atitacharya8 <atitacharya8@gmail.com>
2023-12-12 23:08:53 -0600

    methods in IOrderRepository added

---
+++ Ecommerce_Business/Repository/IRepository/IOrderRepository.cs
@@ -0,0 +1,20 @@
+﻿using Ecommerce_Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce_Business.Repository.IRepository
+{
+    public interface IOrderRepository
+    {
+        public Task<OrderDTO> Get(int id);
+        //get all the order with certain userid or with certain status
+        public Task<IEnumerable<OrderDTO>> GetAll(string? userId = null, string? status = null);
+        public Task<int> Delete(int id);
+        public Task<OrderHeaderDTO> UpdateHeader(OrderHeaderDTO objDTO);
+        public Task<OrderHeaderDTO> MarkPaymentSuccessful(int id);
+        public Task<bool> UpdateOrderStatus(int orderId, string status);
+    }
+}
---
+++ Ecommerce_Business/Repository/OrderRepository.cs
@@ -0,0 +1,14 @@
+﻿using Ecommerce_Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce_Business.Repository
+{
+    public class OrderRepository
+    {
+        
+    }
+}
