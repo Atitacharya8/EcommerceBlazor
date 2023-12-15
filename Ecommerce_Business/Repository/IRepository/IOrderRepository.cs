ddc19c9838d2a5a3c84c90e04bf396d89f3c972c
atitacharya8 <atitacharya8@gmail.com>
2023-12-14 19:02:55 -0600

    Order Create method implemented

--- Ecommerce_Business/Repository/IRepository/IOrderRepository.cs
+++ Ecommerce_Business/Repository/IRepository/IOrderRepository.cs
@@ -9,6 +9,7 @@ namespace Ecommerce_Business.Repository.IRepository
 {
     public interface IOrderRepository
     {
+        public Task<OrderDTO> Create (OrderDTO objDTO);
         public Task<OrderDTO> Get(int id);
         //get all the order with certain userid or with certain status
         public Task<IEnumerable<OrderDTO>> GetAll(string? userId = null, string? status = null);
