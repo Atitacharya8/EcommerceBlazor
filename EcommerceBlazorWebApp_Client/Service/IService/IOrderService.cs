352cd4b83326ecbc974d067981883c03e743cf86
atitacharya8 <atitacharya8@gmail.com>
2023-12-14 22:34:31 -0600

    Order Service Created

---
+++ EcommerceBlazorWebApp_Client/Service/IService/IOrderService.cs
@@ -0,0 +1,12 @@
+﻿using Ecommerce_Models;
+
+namespace EcommerceBlazorWebApp_Client.Service.IService
+{
+    public interface IOrderService
+    {
+        public Task<IEnumerable<OrderDTO>> GetAll(string? userId);
+        public Task<OrderDTO> Get(int orderId);
+
+
+    }
+}
