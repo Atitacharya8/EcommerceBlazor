fef4f562c0fa1515839508b1255fa4b82f52282f
atitacharya8 <atitacharya8@gmail.com>
2023-12-18 20:35:24 -0600

    SignIn API EndPoint Part 1

--- Ecommerce_WebAPI/Controllers/AccountController.cs
+++ Ecommerce_WebAPI/Controllers/AccountController.cs
@@ -69,5 +69,44 @@ namespace Ecommerce_WebAPI.Controllers
             return StatusCode(201); //201 means record has been created and request has been fulfilled
 
         }
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> SignIn([FromBody] SignInRequestDTO signInRequestDTO)
+        {
+            if (signInRequestDTO == null || !ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(signInRequestDTO.UserName, signInRequestDTO.Password, false, false);
+            if (result.Succeeded)
+            {
+                var user = await _userManager.FindByNameAsync(signInRequestDTO.UserName);
+                if (user == null)
+                {
+                    return Unauthorized(new SignInResponseDTO
+                    {
+                        IsAuthSuccessful = false,
+                        ErrorMessage = "Invalid Authentication"
+                    });
+                }
+
+                //everything is valid and we need to login 
+
+
+            }
+            else
+            {
+                return Unauthorized(new SignInResponseDTO
+                {
+                    IsAuthSuccessful = false,
+                    ErrorMessage = "Invalid Authentication"
+                });
+            }
+
+            return StatusCode(201);
+        }
     }
 }
