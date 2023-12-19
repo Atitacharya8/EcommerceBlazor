ee87c2afc9455ed557c9f74ea4949681fb8156ac
atitacharya8 <atitacharya8@gmail.com>
2023-12-18 20:19:47 -0600

    SignUp API Endpoint

--- Ecommerce_WebAPI/Controllers/AccountController.cs
+++ Ecommerce_WebAPI/Controllers/AccountController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Ecommerce_Common;
+using Ecommerce_DataAccess;
+using Ecommerce_Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Ecommerce_WebAPI.Controllers
@@ -20,9 +23,48 @@ namespace Ecommerce_WebAPI.Controllers
             _signInManager = signInManager;
             _roleManager = roleManager;
         }
-        public IActionResult Index()
+        public async Task<IActionResult> SignUp([FromBody] SignUpRequestDTO signUpRequestDTO)
         {
-            return View();
+            if(signUpRequestDTO == null || !ModelState.IsValid) 
+            {
+                return BadRequest();
+            }
+
+            //We should create user, result with user and password, and assign the user a role which is Role_Customer by default
+            var User = new ApplicationUser
+            {
+                UserName = signUpRequestDTO.Email,
+                Email = signUpRequestDTO.Email,
+                Name = signUpRequestDTO.Name,
+                PhoneNumber = signUpRequestDTO.PhoneNumber,
+                EmailConfirmed=true
+
+            };
+
+            var result = await _userManager.CreateAsync(User, signUpRequestDTO.Password);
+
+            if(!result.Succeeded)
+            {
+                return BadRequest(new SignUpResponseDTO()
+                {
+                    IsRegisterationSuccessful = false,
+                    Errors = result.Errors.Select(u=>u.Description)
+                }); ;
+            }
+
+            var roleResult = await _userManager.AddToRoleAsync(User, SD.Role_Customer);
+
+            if (!roleResult.Succeeded)
+            {
+                return BadRequest(new SignUpResponseDTO()
+                {
+                    IsRegisterationSuccessful = false,
+                    Errors = result.Errors.Select(u => u.Description)
+                }); ;
+            }
+
+            return StatusCode(201); //201 means record has been created and request has been fulfilled
+
         }
     }
 }
