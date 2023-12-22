b62ce4a34413cb54bc439b4a9a158d58fdb61baa
atitacharya8 <atitacharya8@gmail.com>
2023-12-22 01:08:46 -0600

    Splitting Code files from .razor to .razor.cs

---
+++ EcommerceBlazorWebApp_Client/Pages/Authentication/Register.razor.cs
@@ -0,0 +1,45 @@
+﻿using Ecommerce_Models;
+using EcommerceBlazorWebApp_Client.Service.IService;
+using Microsoft.AspNetCore.Components;
+using System;
+
+namespace EcommerceBlazorWebApp_Client.Pages.Authentication
+{
+    public partial class Register
+    {
+      
+    private SignUpRequestDTO SignUpRequest = new();
+        public bool IsProcessing { get; set; } = false;
+        public bool ShowRegistrationErrors { get; set; }
+        public IEnumerable<string> Errors { get; set; }
+
+        [Inject]
+        public IAuthenticationService _authSerivce { get; set; }
+
+        [Inject]
+        public NavigationManager _navigationManager { get; set; }
+
+        private async Task RegisterUser()
+        {
+            ShowRegistrationErrors = false;
+            IsProcessing = true;
+
+            //Blazor web assembly runs on the single threaded environment that is why any blocking will results in deadlock of the application so Getawaiter().Getresult() will block the call
+            var result = await _authSerivce.RegisterUser(SignUpRequest);
+            if (result.IsRegisterationSuccessful)
+            {
+                //regiration is successful
+                _navigationManager.NavigateTo("/login");
+            }
+            else
+            {
+                //failure
+                Errors = result.Errors;
+                ShowRegistrationErrors = true;
+
+            }
+            IsProcessing = false;
+        }
+    }
+}
+
