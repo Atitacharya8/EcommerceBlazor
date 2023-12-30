54151a4419c0eef4b7a368d0ec41c12b1ba00274
atitacharya8 <atitacharya8@gmail.com>
2023-12-29 22:33:26 -0600

    Stripe Payment Controller Method

--- Ecommerce_Models/StripePaymentDTO.cs
+++ Ecommerce_Models/StripePaymentDTO.cs
@@ -8,7 +8,14 @@ namespace Ecommerce_Models
 {
     public class StripePaymentDTO
     {
+        public StripePaymentDTO()
+        {
+            SuccessUrl = "OrderConfirmation";
+            CancelUrl = "Summary";
+        }
         public OrderDTO Order { get; set; }
+        public string SuccessUrl { get; set; }
+        public string CancelUrl { get; set; }
 
     }
 }
